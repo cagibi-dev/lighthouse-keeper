bfb7cc78aa3259047a1e97776554ff2f9ea898bb
Paul BLANLOEIL <paul.blanloeil@etu.univ-nantes.fr>
2020-05-12 19:30:57 +0200

    Add sound randomness and bullet trail persistence

--- player/PlayerBullet.gd
+++ player/PlayerBullet.gd
@@ -8,7 +8,18 @@ func _physics_process(delta: float):
 
 func destroy():
 	if not invincible:
-		queue_free()
+		_plan_destruction()
 
 func _on_VisibilityNotifier2D_screen_exited():
+	_plan_destruction()
+
+func _plan_destruction():
+	$Sprite.self_modulate.a = 0
+	$Sprite/Trail.set_deferred("emitting", false)
+	$Light2D.hide()
+	velocity *= 0
+	$CollisionShape2D.set_deferred("disabled", true)
+	$DeadTimer.call_deferred("start")
+
+func _on_DeadTimer_timeout():
 	queue_free()
